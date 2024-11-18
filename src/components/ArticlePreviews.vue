bc2006a951e19c59dc0703b17da32978a0d3303b
Maferep <mf.pont.tv@gmail.com>
2024-11-18 17:19:25 -0300

    post

--- src/components/ArticlePreviews.vue
+++ src/components/ArticlePreviews.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
+import PostPreview from './PostPreview.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
@@ -8,29 +8,29 @@ import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
-  <WelcomeItem>
+  <PostPreview>
     <template #icon>
       <DocumentationIcon />
     </template>
     <template #heading>Handlebars vs. EJS</template>
     I break down my impressions two popular templating tools, cutting through their diverging core philosophies.
-  </WelcomeItem>
+  </PostPreview>
 
-  <WelcomeItem>
+  <PostPreview>
     <template #icon>
       <DocumentationIcon />
     </template>
     <template #heading>Simple and dangerous: Arc's code execution vulnerability</template>
     A hot new browser alternative based on Chromium allowed users to save custom JS code, but a subtle error in their BaaS tooling led to a critical attack vector for their users.
     
-  </WelcomeItem>
+  </PostPreview>
 
-  <WelcomeItem>
+  <PostPreview>
     <template #icon>
       <DocumentationIcon />
     </template>
     <template #heading>My first experience with ORM (Object Relational Mapping)</template>
 
     
-  </WelcomeItem>
+  </PostPreview>
 </template>
